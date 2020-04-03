ece9294e55eea61f474133bbb891d678891f5eac
Martarelli <renanmartarelli@gmail.com>
2020-04-03 01:15:33 -0300

    primeiro commit

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro numero:"))
+num2 = int(input("Digite o segundo numero:"))
+
+if num1 >= num2:
+    print (num1)
+
+else:
+    print(num2)
+
---
+++ teste.py
@@ -0,0 +1,5 @@
+x = 2   
+y = 2
+soma = x+y
+print ("a soma de", x,"e", y,"é igual a", soma)
+
