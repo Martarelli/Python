5e1cb63d6e3b8ab63e5ee702fe929756608241b1
Martarelli <renanmartarelli@gmail.com>
2020-04-16 03:36:43 -0300

    aeeeeeeeeeee

---
+++ Para uso em aula.py
@@ -0,0 +1,21 @@
+"""vetor = [5]
+
+for i in range (1,6):
+    num = int(input("Digite o número %i de 5: "%i))
+    vetor.append(num)
+
+print(vetor)
+
+vetor = []
+vetor2 = []
+for i in range (1,21):
+    if i % 2 != 0:
+        vetor.append(i)
+    else:
+        vetor2.append(i)
+
+print(vetor)
+print(vetor2)"""
+
+
+
