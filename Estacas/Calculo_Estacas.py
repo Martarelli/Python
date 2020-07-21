65462a496e5faad40e214ed74c31ff5a25dcb148
Martarelli <renanmartarelli@gmail.com>
2020-07-21 02:46:54 -0300

    atualizando

--- Estacas/Calculo_Estacas.py
+++ Estacas/Calculo_Estacas.py
@@ -1,35 +1,35 @@
-# importação dos dados de solos e estacas
-
-from Solos.Solos_parametros import *
-from Estaca.Estacas_parametros import *
+from Mod_Estaca import *  # importação dos dados das estacas
+from Mod_Solos import *  # importação dos dados dos solos
 
 
 def recebe_estaca():
     """ATRIBUI OS VALORES DOS FATORES DE PONDERAÇÃO F1(PONTA) E F2(LATERAL) PARA O TIPO DE ESTACA"""
-    print('Franki(F), Metalica(M), Pre_Moldada(PM), Escavada(E), Pre_Moldada_Pqna(PMP)')
-    estaca = input("Qual o tipo de estaca a ser utilizada?: ")
 
 
 def recebe_solo():
     """ATRIBUI OS VALORES DE K E RAZÃO DE ATRITO α PARA O TIPO DE SOLO"""
-
-    print(
-        'AREIAS -> Areia(A), Areia Siltosa(AS), Areia Silto Argilosa(ASA), Areia Argilosa(AA), Areia Argilo Siltosa(AAS)\nSILTES -> Silte(S), Silte Arenoso(SA), '
-        'Silte Areno Argiloso(SAA), Silte Argiloso(SA), Silte Argilo Arenoso(SCA)\nARGILAS -> Argila(C), Argila Arenosa(CA), Argila Areno Siltosa(CAS), '
-        'Argila Siltosa(CS), Argila Silto Arenosa(CSA)')
-    solo = input("Qual o tipo de solo do terreno?: ")
-
-    
-
-    K =  solo(0)
-    alfa = solo(1)
-    return K, alfa
+    print('AREIAS -> Areia(A), Areia Siltosa(AS), Areia Silto Argilosa(ASA), Areia Argilosa(AA), Areia Argilo '
+          'Siltosa(AAS)\nSILTES -> Silte(S), Silte Arenoso(SA), Silte Areno Argiloso(SAA), Silte Argiloso(SA), '
+          'Silte Argilo Arenoso(SCA)\nARGILAS -> Argila(C), Argila Arenosa(CA), Argila Areno Siltosa(CAS), '
+          'Argila Siltosa(CS), Argila Silto Arenosa(CSA)')
+    solo =input("Qual o tipo de solo do terreno?: ").upper()
+    print(solo)
+    print(A[0])
+    print(A[1])
+def calculo():
+    """REALIZA O CALCULO DE RESISTENCIA"""
 
 
 def main():
     """FUNÇÃO PRINCIPAL DO SISTEMA"""
+    print("SEJA BEM VINDO A CALCULADORA DE ESTACAS ELABORADA POR: ENG° RENAN MARTARELLI")
 
-    solo = recebe_solo()
+    recebe_solo()
+    # diam = input("Qual o diâmetro da estaca?(m): ")
+    # prof = input("Qual o a profundidade da estaca?(m): ")
+    # nl = input("Qual o valor do Nspt da camada a ser avaliada?: ")
+    # np = input("Qual o valor do Nspt da camada de apoio da ponta da estaca?: ")
+    # recebe_estaca()
 
 
 main()
---
+++ Estacas/Mod_Estaca.py
@@ -0,0 +1,8 @@
+# diam = 0
+# Franki = [2.50, 5.00]
+# Metalica = [1.75, 3.50]
+# Pre_Moldada = [1.75, 3.50]
+# Escavada = [3.00, 6.00]
+# Pre_Moldada_Pqna = [diam, (diam * 2)]
+# print('Franki(F), Metalica(M), Pre_Moldada(PM), Escavada(E), Pre_Moldada_Pqna(PMP)')
+# estaca = input("Qual o tipo de estaca a ser utilizada?: ").upper()
