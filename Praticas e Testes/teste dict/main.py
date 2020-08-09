a2e3c61d8b51586e16ba1f48e58571e47b80fd12
Martarelli <renanmartarelli@gmail.com>
2020-08-09 19:41:14 -0300

    Teste de Dict - Concluído

--- Praticas e Testes/teste dict/main.py
+++ Praticas e Testes/teste dict/main.py
@@ -1,4 +1,10 @@
 from data import *
 
-solo = input('Digite o solo desejado: ')
-print(solo[0])
+solo_escolha = input('Digite o solo desejado: ')
+se = (solo[solo_escolha])  # O solo escolhido será a chave do dicionário solo
+
+k = se[0]  # k será o primeiro elemento da lista que está no valor da chave do solo escolhido
+alfa = se[1]  # k será o segundo elemento da lista que está no valor da chave do solo escolhido
+
+print(k)
+print(alfa)
